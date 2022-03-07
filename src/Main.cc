26262362f492e849989980c7c25a59945ed1b12a
Azael Reyes <azael.devel@gmail.com>
2022-03-06 19:43:57 -0800

    save

--- src/Main.cc
+++ src/Main.cc
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <string>
 
 #include <chrono>
 using namespace std::chrono;
@@ -11,13 +13,13 @@ using namespace std::chrono;
 #include "saturno.hh"
 
 
-
-struct DataOption
+struct Votacion
 {
-	const char* keys;
+	char* keys;
 	unsigned int length;
+	bool voto;
 
-	bool operator < (const DataOption& d) const
+	bool operator < (const Votacion& d) const
 	{
 		//std::cout << "\t" << index << " < " << d.index << "\n";
 		unsigned int min_length = std::min(length,d.length);
@@ -39,7 +41,7 @@ struct DataOption
 		return false;
 	}
 	
-	bool operator > (const DataOption& d) const
+	bool operator > (const Votacion& d) const
 	{
 		//std::cout << "\t" << index << " < " << d.index << "\n";
 		unsigned int min_length = std::min(length,d.length);
@@ -62,6 +64,10 @@ struct DataOption
 	}
 };
 
+Main::Main() : lengthArray(1000000)
+{
+
+}
 int Main::main(const int argc, const char* argv[])
 {
 	if(strcmp("gen-db",argv[0]) == 0)
@@ -71,11 +77,16 @@ int Main::main(const int argc, const char* argv[])
 			std::cout << "gen-db length_db length_string file\n";
 			return EXIT_FAILURE;
 		}
-		return gen_db(std::stoul(argv[1]),std::stoul(argv[2]),argv[3]);
+		return gen_db(std::stoul(argv[1]),argv[2]);
 	}
 	else if(strcmp("sort-db",argv[0]) == 0)
 	{
-		
+		if(argc < 2) 
+		{
+			std::cout << "sort-db in out\n";
+			return EXIT_FAILURE;
+		}
+		return sort_db(argv[1],argv[2]);		
 	}
 	else if(strcmp("full",argv[0]) == 0)
 	{
@@ -90,10 +101,11 @@ int Main::main(const int argc, const char* argv[])
 	return EXIT_SUCCESS;
 }
 
-int Main::gen_db(unsigned long lengthArray,unsigned int lengthString,const char* filename)
+int Main::gen_db(Index lengthArray,const char* filename)
 {
+	unsigned int lengthString = 32;
 	char** array = new char*[lengthArray];
-	for(unsigned int i = 0; i < lengthArray; i++)
+	for(Index i = 0; i < lengthArray; i++)
 	{
 		array[i] = new char[lengthString + 1];
 		array[i][lengthString] = (char)0;//null terminations string
@@ -101,31 +113,35 @@ int Main::gen_db(unsigned long lengthArray,unsigned int lengthString,const char*
 	
 	std::cout << "Generando base de datos...\n";
 	oct::sat::RandomHash hashs;	
-	for(unsigned int i = 0; i < lengthArray; i++)
+	for(Index i = 0; i < lengthArray; i++)
 	{
+	
 		hashs.generate(array[i],lengthString);
 		//std::cout << array[i] << "\n";
 	}
 	
-	oct::sat::Array<DataOption,unsigned int> arrayData(lengthArray);
-	for(unsigned int i = 0; i < lengthArray; i++)
+	std::default_random_engine generator;
+	std::bernoulli_distribution distribution(0.75);
+	oct::sat::Array<Votacion,Index> arrayData(lengthArray);
+	for(Index i = 0; i < lengthArray; i++)
 	{
 		arrayData[i].keys = array[i];
 		arrayData[i].length = lengthString;
+		arrayData[i].voto = distribution(generator);
 		//std::cout << arrayData[i].index << "\n";
 	}
 	
 	std::ofstream db;	
 	std::cout << "Guardando base de datos...\n";
 	db.open(filename,std::ios::app);
-	for(unsigned int i = 0; i < lengthArray; i++)
+	for(Index i = 0; i < lengthArray; i++)
 	{
-		db << arrayData[i].keys << "\n";
+		db << arrayData[i].keys << "," << arrayData[i].voto << "\n";
 	}
 	db.flush();
 	db.close();
 	
-	for(unsigned int i = 0; i < lengthArray; i++)
+	for(Index i = 0; i < lengthArray; i++)
 	{
 		delete[] array[i];
 	}
@@ -134,14 +150,65 @@ int Main::gen_db(unsigned long lengthArray,unsigned int lengthString,const char*
 	return EXIT_SUCCESS;
 }
 
-int Main::sort_db(const char* filename)
-{
-
+int Main::sort_db(const char* in,const char* out)
+{	
+	std::cout << "in : " << in << "\n";	
+	std::cout << "out : " << out << "\n";
+	
+	std::ifstream infile(in);
+	bool voto;
+	std::string line,field;
+	Index index = 0;
+	oct::sat::Array<Votacion,Index> arrayData(lengthArray);
+	while (std::getline(infile, line))
+	{
+		std::istringstream iss(line);
+				
+		std::getline(iss, field, ',');
+		arrayData[index].keys = new char[field.size()+1];
+		arrayData[index].keys[field.size()] = (char)0;
+		strcpy(arrayData[index].keys, field.c_str());
+		arrayData[index].length = field.size();
+		
+		std::getline(iss, field, ',');
+		voto = (bool)std::stoi(field);
+		//std::cout << person << "," << voto << "\n";
+		index++;
+	}
+	if(index != lengthArray)
+	{
+		std::cout << "Para propositos de medicion la base de datos deve contener exactamente 1 000 000 de registro.\n";
+		return EXIT_FAILURE;
+	}
+	
+	oct::sat::Merge<Votacion,unsigned int> merge(arrayData);
+	std::cout << "Ordenando ascendente...\n";
+	auto begin = high_resolution_clock::now();
+	merge.asc();
+	auto end = high_resolution_clock::now();
+	auto duration = duration_cast<milliseconds>(end - begin);//microseconds	 
+	std::cout << "Duracion : " << duration.count() << "ms\n";
+	
+	std::ofstream outfile;	
+	std::cout << "Guardando base de datos...\n";
+	outfile.open(out,std::ios::app);
+	for(Index i = 0; i < lengthArray; i++)
+	{
+		outfile << arrayData[i].keys << "\n";
+	}
+	outfile.flush();
+	outfile.close();
+	infile.close();	
+	for(Index i = 0; i < lengthArray; i++)
+	{
+		delete[] arrayData[i].keys;
+	}
+	std::cout << "Completado..\n";
+	return EXIT_SUCCESS;
 }
 int Main::full()
 {
 	std::cout << "Iniciando...\n";
-	unsigned int lengthArray = 1000000;
 	unsigned int lengthString = 32;	
 	char** array = new char*[lengthArray];
 	for(unsigned int i = 0; i < lengthArray; i++)
@@ -158,7 +225,7 @@ int Main::full()
 		//std::cout << array[i] << "\n";
 	}
 	
-	oct::sat::Array<DataOption,unsigned int> arrayData(lengthArray);
+	oct::sat::Array<Votacion,unsigned int> arrayData(lengthArray);
 	for(unsigned int i = 0; i < lengthArray; i++)
 	{
 		arrayData[i].keys = array[i];
@@ -168,7 +235,7 @@ int Main::full()
 	//oct::sat::Array<DataOption> arrayDataSorted(lengthArray,false);
 	
 	std::cout << "Ordenando ascendente...\n";
-	oct::sat::Merge<DataOption,unsigned int> merge(arrayData);
+	oct::sat::Merge<Votacion,unsigned int> merge(arrayData);
 	auto begin = high_resolution_clock::now();
 	merge.asc();
 	auto end = high_resolution_clock::now();
--- src/Main.hh
+++ src/Main.hh
@@ -3,17 +3,21 @@
 #define OCTETOS_SATURNO_MAIN_HH
 
 
+typedef unsigned int Index;
+
 class Main
 {
 
 public:
+	Main();
 	int main(const int argc, const char* argv[]);
 
 private:
-	int gen_db(unsigned long,unsigned int,const char*);
+	int gen_db(Index,const char*);
 	int gen_db_default();
-	int sort_db(const char*);
+	int sort_db(const char*,const char*);
 	int full();
+	Index lengthArray;
 };
 
 #endif
