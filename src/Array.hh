1f2ae8b97cabf3072f05744cfc807b62a039f778
Azael <azael.devel@gmail.com>
2022-04-25 18:57:11 -0700

    save

--- src/Array.hh
+++ src/Array.hh
@@ -4,17 +4,17 @@
 
 /*
  * Copyright (C) 2022 Azael R. <azael.devel@gmail.com>
- * 
+ *
  * octetos-saturno is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * octetos-saturno is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -126,7 +126,7 @@ public:
 	{
 		block = new S[length];
 		std::ifstream file("file", std::ios::binary | std::ios::ate);
-		if (not file.read(buffer, length)) throw Exception(Exception::NOT_FOUND_FILE, __FILE__, __LINE__);
+		if (not file.read(block, length)) throw Exception(Exception::NOT_FOUND_FILE, __FILE__, __LINE__);
 	}
 	Block(const Block<S,I>& a)
 	{
