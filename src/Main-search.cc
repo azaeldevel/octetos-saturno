7f85c4a318f9608032f4227a460e250b0ed6df43
Azael Reyes <azael.devel@gmail.com>
2022-04-29 11:48:58 -0700

    save

--- src/Main-search.cc
+++ src/Main-search.cc
@@ -42,13 +42,20 @@ Votacion* Main::search(const std::filesystem::path& db,const char* value)
 		std::cout << "Para propositos de medicion la base de datos deve contener exactamente 1 000 000 (" << engine.get_array().size() << ")  de registro.\n";
 		return NULL;
 	}
-	std::cout << "Buscando..\n";
+	//std::cout << "Buscando..\n";
 	begin = high_resolution_clock::now();
 	Votacion* voto = engine.search(value);
 	end = high_resolution_clock::now();
-	std::cout << ".. resultado\n";
+	//std::cout << ".. resultado\n";
 	duration = duration_cast<milliseconds>(end - begin);
-	std::cout << "Busqueda: " << float(duration.count())/float(1000) << "s\n";
+	if(duration.count() == 0)
+	{
+		std::cout << "Busqueda: menos de un milisegundo\n";
+	}
+	else
+	{
+		std::cout << "Busqueda: " << float(duration.count())/float(1000) << "s\n";
+	}
 	
 	if(voto) return new Votacion(*voto);	
 	return NULL;
